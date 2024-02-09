8b2dd070674b33bb1a810e0248280f25783a7a3e
Ganna Shalaby <140495960+gannashalaby@users.noreply.github.com>
2024-02-09 17:56:42 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtSentence = New TextBox()
+        lstResults = New ListBox()
+        btnParse = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        btnParse2 = New Button()
+        SuspendLayout()
+        ' 
+        ' txtSentence
+        ' 
+        txtSentence.Location = New Point(216, 65)
+        txtSentence.Multiline = True
+        txtSentence.Name = "txtSentence"
+        txtSentence.Size = New Size(432, 75)
+        txtSentence.TabIndex = 0
+        ' 
+        ' lstResults
+        ' 
+        lstResults.FormattingEnabled = True
+        lstResults.Location = New Point(216, 192)
+        lstResults.Name = "lstResults"
+        lstResults.Size = New Size(432, 124)
+        lstResults.TabIndex = 1
+        ' 
+        ' btnParse
+        ' 
+        btnParse.Location = New Point(216, 364)
+        btnParse.Name = "btnParse"
+        btnParse.Size = New Size(122, 38)
+        btnParse.TabIndex = 2
+        btnParse.Text = "Lexical Analysis"
+        btnParse.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(52, 65)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(117, 20)
+        Label1.TabIndex = 4
+        Label1.Text = "Enter Expression"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(52, 192)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(55, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "Results"
+        ' 
+        ' btnParse2
+        ' 
+        btnParse2.Location = New Point(455, 364)
+        btnParse2.Name = "btnParse2"
+        btnParse2.Size = New Size(125, 38)
+        btnParse2.TabIndex = 6
+        btnParse2.Text = "Syntax Analysis"
+        btnParse2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnParse2)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btnParse)
+        Controls.Add(lstResults)
+        Controls.Add(txtSentence)
+        Name = "Form1"
+        Text = "Mini Project"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtSentence As TextBox
+    Friend WithEvents lstResults As ListBox
+    Friend WithEvents btnParse As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents btnParse2 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Linq.Expressions
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Function IsDigit(ByVal c As Char) As Boolean
+        Return Asc(c) >= Asc("0") AndAlso Asc(c) <= Asc("9")
+    End Function
+    Function IsAlphabet(ByVal c As Char) As Boolean
+        Return (Asc(c) >= Asc("A") AndAlso Asc(c) <= Asc("Z")) OrElse (Asc(c) >= Asc("a") AndAlso Asc(c) <= Asc("z"))
+    End Function
+
+    Function IsWhiteSpace(ByVal c As Char) As Boolean
+        Return c = " " OrElse c = Chr(13) OrElse c = Chr(10)
+    End Function
+
+    Function IsValidOperator(ByVal c As Char) As Boolean
+        Return c = "+" OrElse c = "-" OrElse c = "*" OrElse c = "/" OrElse c = "="
+    End Function
+
+    Function IsDigitOrAlphabet(ByVal c As Char) As Boolean
+        Return IsDigit(c) OrElse IsAlphabet(c)
+    End Function
+
+    Function ParseOperand(ByRef i As Integer, ByVal expression As String) As String
+        Dim operand As String = ""
+
+        While i < expression.Length AndAlso IsDigitOrAlphabet(expression(i))
+            operand &= expression(i)
+            i += 1
+        End While
+
+        Return operand
+    End Function
+
+
+    Function ParseOperator(ByRef i As Integer, ByVal expression As String) As Char
+        If i < expression.Length AndAlso IsValidOperator(expression(i)) Then
+            Dim oper As Char = expression(i)
+            i += 1
+            Return oper
+        Else
+            Throw New Exception("Invalid or missing operator.")
+        End If
+    End Function
+
+    Sub SkipWhiteSpaces(ByRef i As Integer, ByVal expression As String)
+        While i < expression.Length AndAlso IsWhiteSpace(expression(i))
+            i += 1
+        End While
+    End Sub
+
+    Private Sub btnParse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnParse.Click
+        Dim i As Integer = 0
+        Dim expression As String = txtSentence.Text
+        Dim token As String
+        Dim tokenCount As Integer = 0
+
+        lstResults.Items.Clear()
+
+        SkipWhiteSpaces(i, expression)
+
+        While i < expression.Length
+            If IsDigitOrAlphabet(expression(i)) Then
+                token = ParseOperand(i, expression)
+                tokenCount += 1
+                lstResults.Items.Add("Token no" & tokenCount.ToString() & ": " & token)
+            ElseIf IsValidOperator(expression(i)) Then
+                token = ParseOperator(i, expression)
+                tokenCount += 1
+                lstResults.Items.Add("Token no" & tokenCount.ToString() & ": " & token)
+            ElseIf IsWhiteSpace(expression(i)) Then
+                SkipWhiteSpaces(i, expression)
+                Continue While
+            Else
+                token = expression(i)
+                tokenCount += 1
+                lstResults.Items.Add("Token no" & tokenCount.ToString() & ": " & token)
+                i += 1
+            End If
+        End While
+
+        lstResults.Items.Add("Number of tokens: " & tokenCount)
+    End Sub
+
+    Private Function IsValidExpression(ByVal expression As String) As Boolean
+        Dim i As Integer = 0
+        Dim valid As Boolean = True
+
+        Try
+            SkipWhiteSpaces(i, expression)
+
+            If i < expression.Length AndAlso IsDigitOrAlphabet(expression(i)) Then
+                ParseOperand(i, expression)
+            Else
+                Return False
+            End If
+
+            While i < expression.Length
+                SkipWhiteSpaces(i, expression)
+
+                If i < expression.Length AndAlso IsValidOperator(expression(i)) Then
+                    ParseOperator(i, expression)
+                Else
+                    Return False
+                End If
+
+                SkipWhiteSpaces(i, expression)
+
+                If i < expression.Length AndAlso IsDigitOrAlphabet(expression(i)) Then
+                    ParseOperand(i, expression)
+                Else
+                    Return False
+                End If
+            End While
+
+        Catch ex As Exception
+            valid = False
+        End Try
+
+        Return valid
+    End Function
+
+    Private Sub btnParse2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnParse2.Click
+        Dim expression = txtSentence.Text
+        Dim result As String
+
+        If IsValidExpression(expression) Then
+            result = "Valid"
+        Else
+            result = "Invalid"
+        End If
+
+        lstResults.Items.Clear()
+        lstResults.Items.Add(result)
+    End Sub
+End Class
